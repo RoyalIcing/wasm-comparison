20d47e28723c3b421ce78080817ec8612658ba39
Patrick Smith <pgwsmith@gmail.com>
2022-07-10 23:18:25 +1000

    Shorten state machine code

--- state-machine-deno.mjs
+++ state-machine-deno.mjs
@@ -1,7 +1,9 @@
-const memory = new WebAssembly.Memory({
-  initial: 65536,
-  maximum: 65536,
-})
+const decodeUTF8 = new TextDecoder();
+
+// const memory = new WebAssembly.Memory({
+//   initial: 65536,
+//   maximum: 65536,
+// });
 
 WebAssembly.instantiate(await Deno.readFile("state-machine-zig.wasm"), {
   env: {
@@ -12,12 +14,16 @@ WebAssembly.instantiate(await Deno.readFile("state-machine-zig.wasm"), {
 }).then((result) => {
   const memory = new Uint8Array(result.instance.exports.memory.buffer);
 
-  console.log(result.instance.exports);
-  console.log(result.instance.exports.current);
-  console.log(result.instance.exports.current.value);
+  function getStringAt(ptr) {
+    const endPtr = memory.indexOf(0, ptr);
+    return decodeUTF8.decode(memory.subarray(ptr, endPtr));
+  }
+
+  console.log(getStringAt(result.instance.exports.toString()));
   console.log(memory[result.instance.exports.current.value]);
   
-  console.log(result.instance.exports.flick());
-  console.log(result.instance.exports.current.value);
+  result.instance.exports.flick();
+
+  console.log(getStringAt(result.instance.exports.toString()));
   console.log(memory[result.instance.exports.current.value]);
 });
--- state-machine.zig
+++ state-machine.zig
@@ -1,6 +1,5 @@
 const mem = @import("std").mem;
 const ComptimeStringMap = @import("std").ComptimeStringMap;
-const EnumArray = @import("std").EnumArray;
 
 const Switch = struct {
     pub const State = enum(u8) {
@@ -11,52 +10,34 @@ const Switch = struct {
     pub const Event = enum {
         flick,
 
-        pub fn apply(self: Event, state: State) State {
+        pub fn transition(self: Event, state: State) State {
             switch (self) {
                 .flick => {
                     return switch (state) {
                         State.off => State.on,
                         State.on => State.off,
                     };
-                }
+                },
             }
         }
     };
 
-    fn flick(self: Switch) Switch {
-        var copy = self;
-        copy.state = switch (self.state) {
-            State.off => State.on,
-            State.on => State.off,
-        };
-        return copy;
-    }
-
-    const EventHandlers = ComptimeStringMap(fn (Switch) Switch, .{.{ @tagName(Event.flick), Switch.flick }});
-
     const Events = ComptimeStringMap(Event, .{.{ @tagName(Event.flick), .flick }});
-    const EventHandlers2 = EnumArray(Event, fn (Switch) Switch).init(.{ .flick = Switch.flick });
 
     state: State,
 
-    pub fn next(self: Switch, event: [:0]const u8) Switch {
-        if (Events.get(event)) |e| {
+    pub fn next(self: Switch, eventName: [:0]const u8) Switch {
+        if (Events.get(eventName)) |event| {
             var copy = self;
-            copy.state = e.apply(self.state);
+            copy.state = event.transition(self.state);
             return copy;
         }
-        // if (EventHandlers.get(event)) |h| {
-        //     return h(self);
-        // }
-        // if (Events.get(event)) |v| {
-        //     return EventHandlers2.get(v)(self);
-        // }
 
         return self;
     }
 };
 
-export var current = Switch{ .state = Switch.State.off };
+export var current = Switch{ .state = .off };
 
 export fn flick() void {
     current = current.next("flick");
