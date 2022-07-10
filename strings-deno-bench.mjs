d2a1ae187ade016ae96095f3bb1ec6498d081267
Patrick Smith <pgwsmith@gmail.com>
2022-07-10 20:25:45 +1000

    More string benchmarks

--- strings-deno-bench.mjs
+++ strings-deno-bench.mjs
@@ -2,7 +2,7 @@ import * as JS from "./strings.js";
 
 const { instance } = await WebAssembly.instantiate(await Deno.readFile("strings-zig.wasm"))
 
-Deno.bench("wasm", () => {
+Deno.bench("wasm, new TextDecoder", () => {
   const htmlBeginEn = instance.exports.htmlBeginEn;
   
   const memory = new Uint8Array(instance.exports.memory.buffer);
@@ -14,7 +14,7 @@ Deno.bench("wasm", () => {
 });
 
 const utf8decoder = new TextDecoder();
-Deno.bench("wasm 2", () => {
+Deno.bench("wasm, reuse TextDecoder", () => {
   const htmlBeginEn = instance.exports.htmlBeginEn;
   
   const memory = new Uint8Array(instance.exports.memory.buffer);
@@ -24,6 +24,26 @@ Deno.bench("wasm 2", () => {
   const string = utf8decoder.decode(memory.subarray(ptr, endPtr));
 });
 
+Deno.bench("wasm, then join strings in JavaScript", () => {
+  const htmlBeginEn = instance.exports.htmlBeginEn;
+  
+  const memory = new Uint8Array(instance.exports.memory.buffer);
+  const ptr = htmlBeginEn();
+  const endPtr = memory.indexOf(0, ptr);
+  
+  const string1 = utf8decoder.decode(memory.subarray(ptr, endPtr));
+  const string2 = utf8decoder.decode(memory.subarray(ptr, endPtr));
+  const string3 = utf8decoder.decode(memory.subarray(ptr, endPtr));
+  [string1, string2, string3].join("\n");
+});
+
 Deno.bench("javascript module", () => {
   const string = JS.htmlBeginEn();
+});
+
+Deno.bench("javascript module, then join strings in JavaScript", () => {
+  const string1 = JS.htmlBeginEn();
+  const string2 = JS.htmlBeginEn();
+  const string3 = JS.htmlBeginEn();
+  [string1, string2, string3].join("\n");
 });
